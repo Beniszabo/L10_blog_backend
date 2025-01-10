6f958a32345e4bd7e35eec1a7e11a0b2dfa2dabb
Beniszabo <szabobenji60@gmail.com>
2025-01-10 11:23:56 +0100

    loginlogout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,9 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
     public function register(Request $request){
 
@@ -16,15 +17,59 @@ class AuthController extends Controller
                 'name'=>'required',
                 'email'=>'required|email|unique:App\Models\User,email',
                 'password'=>'required',
+                'confirm_password'=>'required|same:password',
                 'role'=>'required|integer'
+            ],
+            [
+                'name.required'=>"Kötelező kitölteni!",
+                'email.required'=>"Kötelező kitölteni!",
+                'email.email'=>"Hibás email cím!",
+                'email.unique'=>"Az email cím már létezik!",
+                'password.required'=>"Kötelező kitölteni!",
+                'confirm_password.required'=>"Kötelező kitölteni!",
+                'confirm_password.same'=>"A két jelszó nem egyforma!",
+                'role.required'=>"Kötelező kitölteni!",
+                'role.integer'=>"Csak szám lehet!"
+
             ]
         );
         if ($validator->fails()) {
-            return response()->json($validator->errors(),400);
+            return $this->sendError('Hibás adatok!',$validator->errors(),400);
         }
         $input=$request->all();
         $input['password']=bcrypt($input['password']);
         $user=User::create($input);
-        return response()->json($input,201);
+
+        $response=[
+            'name'=>$user->name,
+            'token'=>$user->createToken('Secret')->plainTextToken
+        ];
+
+        return $this->sendResponse($response,'Sikeres regisztráció!',201);
+    }
+
+    public function login(Request $request){
+        if (Auth::attempt([
+            'email'=>$request->email,
+            'password'=>$request->password
+        ])) {
+            //sikeres bejelentkezés
+            $user=Auth::user(); //user összes adata
+            $response=[
+                'name'=>$user->name,
+                'token'=>$user->createToken('Secret')->plainTextToken,
+                'id'=>$user->id,
+                'role'=>$user->role
+            ];
+            return $this->sendResponse($response,'Sikeres bejelentkezés!',201);
+        }
+        else{
+            return $this->sendError(['','error'=>'Sikertelen bejelentkezés!'],401);
+        }
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return $this->sendResponse('','Sikeres kijelentkezés!');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BaseController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,5 +10,15 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class,'register']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
 
+Route::any('have-to-login',function(){
+    //return response()->json('Bejelentkezés szükséges',401);
+    $bc =new BaseController();
+    return $bc ->sendError('Bejelentkezés szükséges','',401);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
